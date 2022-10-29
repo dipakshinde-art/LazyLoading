3109e466d889845d6e3c9b875748571de7f586ff
Dipak shinde <deepakshinde3346@gmail.com>
2022-10-29 21:48:04 +0530

    folder structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MainRoute from './Components/AllRoute/MainRoute';
+
+
+function App() {
+  return (
+    <>
+
+      <MainRoute />
+    </>
+  );
+}
+
+export default App;
